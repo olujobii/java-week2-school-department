68ac7cee224dd56a1204cc346a743d11d0c2a799
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-17 06:13:59 +0100

    Principal implementing AdminPriviledges to enroll students, epel students, approve course addition, remove course addition

--- src/main/java/com/olujobii/AdminPriviledges.java
+++ src/main/java/com/olujobii/AdminPriviledges.java
@@ -1,4 +1,14 @@
 package com.olujobii;
 
+import java.util.List;
+
 public interface AdminPriviledges {
+
+    boolean enrollStudent(Applicant applicant);
+
+    boolean expelStudent(Student student);
+
+    boolean approveCourseAddition(Course course, List<Course> availableCourses);
+
+    boolean approveCourseRemoval(Course course, List<Course> availableCourses);
 }
--- src/main/java/com/olujobii/Principal.java
+++ src/main/java/com/olujobii/Principal.java
@@ -1,10 +1,27 @@
 package com.olujobii;
 
-public class Principal extends Staff{
+import java.util.ArrayList;
+
+public class Principal extends Staff implements AdminPriviledges{
 
     public Principal(String name, int age, String id, double salary) {
         super(name, age, id, salary);
     }
 
+    /*
+     * We can use an interface because not only principal should be able to approve for removal
+     * or addition of course to curriculum, to expel student upon wrong-doing or to enroll a
+     * student
+     * */
+    @Override
+    public boolean enrollStudent(Applicant applicant){
+        if(applicant.getAge() < 11)
+            return false;
 
+        Student student = new Student(applicant.getName(),
+                                        applicant.getAge(),
+                                        applicant.getId(), new ArrayList<>());
+        //We still have to add new Student to the database(arrayList) of enrolled student
+        return true;
+    }
 }
