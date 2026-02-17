cd8f75dcc52b3f44a015a4529f30e01f716ee414
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-17 06:14:32 +0100

    Created Applicant class

--- src/main/java/com/olujobii/Applicant.java
+++ src/main/java/com/olujobii/Applicant.java
@@ -1,4 +1,8 @@
 package com.olujobii;
 
-public class Applicant {
+public class Applicant extends Person{
+
+    public Applicant(String name, int age, String id) {
+        super(name, age, id);
+    }
 }
