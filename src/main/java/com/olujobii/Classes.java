c229b88606f507521cee90c71f48e41edaaa7230
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-17 06:14:51 +0100

    Created Classes class

--- src/main/java/com/olujobii/Classes.java
+++ src/main/java/com/olujobii/Classes.java
@@ -1,4 +1,13 @@
 package com.olujobii;
 
+import java.util.List;
+
 public class Classes {
+    private final List<Student> classStudentsList;
+    private Teacher classTeacher;
+
+    public Classes(List<Student> classStudentsList, Teacher classTeacher){
+        this.classStudentsList = classStudentsList;
+        this.classTeacher = classTeacher;
+    }
 }
