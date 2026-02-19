b8d1de9e3949331a2ccea472a9515bc849263905
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-19 04:56:49 +0100

    Made changes to salary data type in Constructor

--- src/main/java/com/olujobii/NonAcademicStaff.java
+++ src/main/java/com/olujobii/NonAcademicStaff.java
@@ -2,7 +2,7 @@ package com.olujobii;
 
 public class NonAcademicStaff extends Staff{
 
-    public NonAcademicStaff(String name, int age, String id, double salary) {
+    public NonAcademicStaff(String name, int age, String id, int salary) {
         super(name, age, id, salary);
     }
 
