8325281cb0b3284e563257cf8cefe91bb136d3f1
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-19 18:14:45 +0100

    Working on functionality to approve applicant and performed safety guard and list options

--- src/main/java/com/olujobii/Department.java
+++ src/main/java/com/olujobii/Department.java
@@ -23,7 +23,7 @@ public class Department {
             System.out.println("2. List total number of students");
             System.out.println("3. List total number of staffs");
             System.out.println("4. Register as an applicant");
-            System.out.println("5. Approve or deny an applicant");
+            System.out.println("5. Approve an applicant");
             System.out.println("6. Expel a student");
             System.out.println("7. Exit");
             System.out.print("Enter an option: ");
@@ -43,7 +43,7 @@ public class Department {
                     registerApplicant();
                     break;
                 case "5":
-                    System.out.println("APPROVE OR DENY AN APPLICANT");
+                    approveApplicant();
                     break;
                 case "6":
                     System.out.println("Expel a student");
@@ -61,6 +61,12 @@ public class Department {
 
     private void listAvailableCourses(){
         List<Course> courses = departmentDatabase.getAvailableCourses();
+
+        if(courses.isEmpty()){
+            System.out.println("We offer no courses at the moment");
+            return;
+        }
+
         System.out.println("We have "+courses.size()+" available courses");
 
         for(Course course : courses){
@@ -70,6 +76,12 @@ public class Department {
 
     private void listAllStudents(){
         List<Student> students = departmentDatabase.getEnrolledStudents();
+
+        if(students.isEmpty()){
+            System.out.println("We have no student at the moment");
+            return;
+        }
+
         System.out.println("We have "+students.size()+" enrolled students");
 
         for(Student student : students){
@@ -79,6 +91,12 @@ public class Department {
 
     private void listAllStaffs(){
         List<Staff> staffs = departmentDatabase.getAvailableStaffs();
+
+        if(staffs.isEmpty()){
+            System.out.println("We have no staffs at the moment");
+            return;
+        }
+
         System.out.println("We have "+staffs.size()+" available staffs");
 
         for(Staff staff : staffs){
@@ -87,7 +105,7 @@ public class Department {
     }
 
     private void registerApplicant(){
-        String applicantName = "";
+        String applicantName;
         do {
             System.out.print("Enter your name: ");
             applicantName = scanner.nextLine();
@@ -122,9 +140,68 @@ public class Department {
         System.out.println("Your profile has been created.");
     }
 
+    private void approveApplicant(){
+        if(!adminAuthentication()){
+            System.out.println("Cannot sign in at the moment. Try again later");
+            return;
+        }
+
+        System.out.println("Welcome back admin");
+        System.out.println("Which applicants will you like to approve: ");
+        listAllApplicants();
+    }
+
     //Creating 3 digits random ID for applicant
     private int createRandomID(){
         Random random = new Random();
         return random.nextInt(1,1000);
     }
+
+    private boolean adminAuthentication(){
+        int noOfChances = 3; //No of Chances to input correct password
+        String userPasswordInput;
+        AdminPriviledges adminPriviledges = departmentDatabase.getAdminPriviledgesType();
+
+        if(adminPriviledges == null)
+            return false;
+
+
+        //ADMIN PASSWORD IS 12345
+        do{
+            System.out.println(noOfChances+"/3 attempts");
+            System.out.print("Enter admin password to gain access: ");
+            userPasswordInput = scanner.nextLine();
+
+            if(!userPasswordInput.equals("12345")){
+                System.out.println("Password incorrect");
+                noOfChances--;
+            }
+
+            if(userPasswordInput.equals("12345")){
+                break;
+            }
+        }while(noOfChances > 0);
+
+        if(noOfChances == 0){
+            return false;
+        }
+
+        return true;
+    }
+
+    private void listAllApplicants(){
+        List<Applicant> applicants = departmentDatabase.getApplicantList();
+
+        if(applicants.isEmpty()){
+            System.out.println("We have no applicants at the moment");
+            return;
+        }
+
+        for(int i = 0 ; i < applicants.size() ; i++){
+            int order = i + 1;
+
+            System.out.println(order+". "+applicants.get(i));
+        }
+    }
+
 }
