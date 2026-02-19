1a9cd7f62e54db54a03e069e8f72d57db8470b26
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-19 04:47:34 +0100

    created mockData()

--- src/main/java/com/olujobii/Main.java
+++ src/main/java/com/olujobii/Main.java
@@ -1,17 +1,37 @@
 package com.olujobii;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.olujobii.repository.DepartmentDatabase;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Department department = createMockData();
+        department.runApplication();
+    }
+
+    private static Department createMockData(){
+        //-----CREATING MOCK DATA-----
+        //Courses
+        Course english = new Course("English", "ENG-201");
+        Course maths = new Course("Mathematics","MATH-202");
+        Course java = new Course("Introduction to Java","INJ-101");
+        Course physics = new Course("Physics","PHY-102");
+
+        //Principal
+        AdminPriviledges adminPriviledges = new Principal("Mr. Olujobi",55,"PR-3412",500_000);
+
+        //Staffs
+        Staff teacher1 = new Teacher("Mr. Benson",39,"TCH-892",150_000,physics);
+        Staff teacher2 = new Teacher("Mr. Deji",45,"TCH-491",200_000,java);
+        Staff teacher3 = new Teacher("Mrs. Glory",42,"TCH-081",200_000,english);
+        Staff teacher4 = new Teacher("Miss. Praise",39,"TCH-801",180_000,maths);
+        Staff nonAcademicStaff = new NonAcademicStaff("Mr.Benjamin",35,"NASTF-742",100_000);
+
+        DepartmentDatabase departmentDatabase = new DepartmentDatabase(new ArrayList<Student>(),new ArrayList<Staff>()
+                ,new ArrayList<Applicant>(), new ArrayList<Course>());
+
+        return new Department(departmentDatabase,adminPriviledges);
     }
 }
