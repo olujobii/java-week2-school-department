389f98947bb12e0376eed0224906b61e73f928ef
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-24 06:05:55 +0100

    Created method runApplication() and exitApplication()

--- src/main/java/com/olujobii/controller/SchoolMgtSystemController.java
+++ src/main/java/com/olujobii/controller/SchoolMgtSystemController.java
@@ -10,6 +10,51 @@ public class SchoolMgtSystemController {
     }
 
     public void runApplication(){
+        boolean isProgramRunning = true;
+        while (isProgramRunning){
+            System.out.println("\n==== SCHOOL DEPARTMENT APP ====");
+            System.out.println("1. List total number of courses");
+            System.out.println("2. List total number of students");
+            System.out.println("3. List total number of staffs");
+            System.out.println("4. Register as an applicant");
+            System.out.println("5. Approve an applicant");
+            System.out.println("6. Expel a student");
+            System.out.println("7. Exit");
+            System.out.print("Enter an option: ");
 
+            String userOption = scanner.nextLine();
+            switch (userOption){
+                case "1":
+                    System.out.println("Listed courses");
+                    break;
+                case "2":
+                    System.out.println("Listed students");
+                    break;
+                case "3":
+                    System.out.println("Listed staffs");
+                    break;
+                case "4":
+                    System.out.println("Registered applicants");
+                    break;
+                case "5":
+                    System.out.println("Approved applicants");
+                    break;
+                case "6":
+                    System.out.println("Expelled Student");
+                    break;
+                case "7":
+                    exitApplication();
+                    isProgramRunning = false;
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+                    break;
+            }
+        }
+    }
+
+    private void exitApplication(){
+        System.out.println("Thanks for using our app");
+        scanner.close();
     }
 }
