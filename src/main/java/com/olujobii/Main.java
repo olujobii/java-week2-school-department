dd9dffa765f0ee83132d87ed9a88dd1149ef0e2e
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2026-02-24 06:06:15 +0100

    Initializing SchoolMgtSystemController

--- src/main/java/com/olujobii/Main.java
+++ src/main/java/com/olujobii/Main.java
@@ -1,4 +1,15 @@
 package com.olujobii;
 
+import com.olujobii.controller.SchoolMgtSystemController;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        SchoolMgtSystemController schoolMgtSystemController = new SchoolMgtSystemController(new Scanner(System.in));
+
+        schoolMgtSystemController.runApplication();
+
+    }
 }
